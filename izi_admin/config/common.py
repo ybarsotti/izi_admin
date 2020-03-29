b2d760dec011f33f5fd7677c936baaf3fdcd19e3
Yuri Barsotti <yuri@weef.com.br>
2020-03-29 16:18:46 -0300

    Correção staticfiles

--- izi_admin/config/common.py
+++ izi_admin/config/common.py
@@ -140,6 +140,8 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
     STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
     # Graphene
     GRAPHENE = {
