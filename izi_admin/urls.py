d4281eb9ba6584e80f4093e74da18a585324163b
Yuri Barsotti <yuri@weef.com.br>
2020-03-29 16:28:36 -0300

    Adicionado url de statics

--- izi_admin/urls.py
+++ izi_admin/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
+
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
